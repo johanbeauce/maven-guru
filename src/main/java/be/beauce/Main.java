248631a96837bcd5b3b834f118a2eac66edf8275
Johan BEAUCE <jbeauce@pictet-technologies.com>
2023-06-24 11:32:54 +0200

    Add maven dependency

--- src/main/java/be/beauce/Main.java
+++ src/main/java/be/beauce/Main.java
@@ -1,7 +1,9 @@
 package be.beauce;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(StringUtils.upperCase("Hello world!"));
     }
 }
